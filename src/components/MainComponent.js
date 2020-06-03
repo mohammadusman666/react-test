8040da9dea64d774308667e0e54576f8791c05dc
Mohammad Usman <46886908+mohammadusman666@users.noreply.github.com>
2020-06-03 20:28:05 +0500

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Header from '../components/HeaderComponent';
-import Footer from '../components/FooterComponent';
-import Menu from '../components/MenuComponent';
-import DishDetail from '../components/DishDetailComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Menu from './MenuComponent';
+import Home from './HomeComponent';
+import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -12,22 +15,29 @@ class Main extends Component {
         
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            // selectedDish: null
         }
     }
 
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
-    }
+    // onDishSelect(dishId) {
+    //     this.setState({selectedDish: dishId});
+    // }
 
     render() {
+        const HomePage = () => {
+            return(
+                <Home />
+            );
+        }
+
         return (
             <div className="App">
                 <Header />
-                <Menu dishes={this.state.dishes}
-                    onClick={(dishId) => this.onDishSelect(dishId)} />
-                
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer />
             </div>
         );
